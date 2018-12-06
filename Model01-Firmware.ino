5be39343ff9a20b01dfd913ec406e6c63041fbdb
Eric Schmidt <eric.schmidt@oneiro.io>
2018-12-06 14:19:26 -0800

    Remap shifts to pinkies

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -214,18 +214,18 @@ KEYMAPS(
 #elif defined (PRIMARY_KEYMAP_CUSTOM)
   // Edit this keymap to make a custom layout
   [PRIMARY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+  (___,           Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+   Key_Backtick,  Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
+   Key_Backslash, Key_A, Key_S, Key_D, Key_F, Key_G,
+   Key_LeftShift, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+   Key_LeftControl, Key_Backspace, Key_LeftGui, ShiftToLayer(FUNCTION),
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_QUOTE),Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Backslash,
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftGui, Key_Spacebar, Key_RightControl,
+   M(MACRO_QUOTE), Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Minus,
+   Key_Enter,      Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
+                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
+   Key_RightAlt,   Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_RightShift,
+   ShiftToLayer(FUNCTION), Key_LeftGui, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
 #else
